711ab4c2002841824737336a08c132d37e01f5a9
SwathyJagannatha <swathyjagannatha308@gmail.com>
2024-09-12 21:38:53 -0700

    App.py Changes

--- app.py
+++ app.py
@@ -60,14 +60,8 @@ def rate_limit_config():
 # app = create_app('DevelopmentConfig')
 
 # if __name__ == '__main__':
-    app = create_app('ProductionConfig')
+app = create_app('ProductionConfig')
 
-    blueprint_config(app)
-
-    rate_limit_config()
-
-    with app.app_context():
-        #db.drop_all()
-        db.create_all()
-
-    app.run(debug=True)
+with app.app_context():
+    #db.drop_all()
+    db.create_all()
