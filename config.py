7d098adb7808adeece97ccf70c5183898b1708a3
SwathyJagannatha <swathyjagannatha308@gmail.com>
2024-09-15 13:49:36 -0700

    Update config.py

--- config.py
+++ config.py
@@ -9,10 +9,3 @@ class ProductionConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///app.db'
     CACHE_TYPE = 'SimpleCache'
     DEBUG = True
-
-     # Mail configuration
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587 #465
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = 'swaj718@gmail.com'
-    MAIL_PASSWORD = 'aunf gvkq wsfe pndd'
