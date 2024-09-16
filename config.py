b3a8352dec254297f7b86bc491246c3f59c4b25f
SwathyJagannatha <swathyjagannatha308@gmail.com>
2024-09-15 19:10:23 -0700

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,10 @@ class ProductionConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///app.db'
     CACHE_TYPE = 'SimpleCache'
     DEBUG = True
+
+# Configuring mail settings
+    MAIL_SERVER = 'smtp.sendgrid.net'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('SENDGRID_USERNAME', 'apikey')  # Defaults to 'apikey' if not set
+    MAIL_PASSWORD = os.environ.get('SENDGRID_PASSWORD')  # No default to ensure security
